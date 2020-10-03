4ca00149fd585c33f2ae73622b836bff4876c22a
Brick Ellis <brickellis@yahoo.com>
2020-10-02 18:25:58 -0600

    Add files via upload

---
+++ linkcollector.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+sites = open("/Users/Brick/Desktop/General Conference Project/sites.txt","r").readlines()
+pattern = re.compile(r"\"(?P<link>.*)\?.*class=\"lumen-tile__link\"")
+i = 0
+for site in sites:
+    i += 1
+    site = site.replace("\n", "")
+    response = requests.get(site).text
+    talks = re.findall(pattern, response)
+    talkdoc = open("/Users/Brick/Desktop/General Conference Project/talks.txt","a") 
+    for talk in talks:
+        talkdoc.write(talk + '\n')
+    talkdoc.close()
+    print(str(i) + " is done.")
